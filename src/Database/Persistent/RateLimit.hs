3ece680766d6fb250471f699f56635bbc07bdc27
jp.rider63 <dev@jamesparker.me>
2014-12-11 11:48:44 -0500

    added rateLimitFilters and some comments

--- src/Database/Persistent/RateLimit.hs
+++ src/Database/Persistent/RateLimit.hs
@@ -8,10 +8,20 @@ import Yesod
 class RateLimit action entity | action -> entity where
     -- | Number of actions allowed in the defined period. Period is easured in seconds. 
     rateLimit :: action -> (Int, Int)
-    convertAction :: action -> UTCTime -> entity
-    timeConstructor :: action -> EntityField entity UTCTime
---    retrieveActionCount :: action -> IO Int -- maybe just constructors to get the correct db values??
 
+    -- | Convert a given action and time into an entity. 
+    convertAction :: action -> UTCTime -> entity
+
+    -- | Return the field for the time constructor in the entity. 
+    timeConstructor :: action -> EntityField entity UTCTime
+
+    -- | Optionally add additional filters for database queries. 
+    -- The default is `[]`. 
+    -- You probably only need this if multiple rate limiters use the same entity. 
+    rateLimitFilters :: action -> [Filter entity]
+    rateLimitFilters _ = []
+
+-- | Returns the number of actions remaining for the current period. 
 numberOfRemainingActions :: (RateLimit action entity, 
         PersistEntityBackend entity ~ YesodPersistBackend site, 
         YesodPersist site, 
@@ -21,11 +31,13 @@ numberOfRemainingActions :: (RateLimit action entity,
 numberOfRemainingActions action = do
     let ( limit, period) = rateLimit action
     let timeConstr = timeConstructor action
+    let filters = rateLimitFilters action
     now <- lift getCurrentTime
     let timeBound = addUTCTime (fromIntegral $ negate period) now
-    c <- runDB $  count [ timeConstr >. timeBound]
+    c <- runDB $  count $ (timeConstr >. timeBound):filters
     return $ limit - c
 
+-- | Determines whether an actions can be performed. 
 canPerformAction :: (RateLimit action entity,
         PersistEntityBackend entity ~ YesodPersistBackend site, 
         YesodPersist site, 
@@ -35,6 +47,7 @@ canPerformAction :: (RateLimit action entity,
 canPerformAction action = 
     numberOfRemainingActions action >>= return . (> 0)
 
+-- | Record when an action occurs. 
 recordAction :: (RateLimit action entity,
         PersistEntityBackend entity ~ YesodPersistBackend site, 
         YesodPersist site, 
@@ -57,6 +70,7 @@ cleanOldActions :: (RateLimit action entity,
 cleanOldActions action = do
     let ( _, period) = rateLimit action
     let timeConstr = timeConstructor action
+    let filters = rateLimitFilters action
     now <- lift getCurrentTime
     let timeBound = addUTCTime (fromIntegral $ negate period) now
-    runDB $ deleteWhere [ timeConstr <=. timeBound]
+    runDB $ deleteWhere $ (timeConstr <=. timeBound):filters
